f3a654f68b5ded61a1c8f8e6ec9d9d27ba1039b1
Adam King <rak@us.ibm.com>
2019-07-31 15:21:14 -0400

    Use kubernetes-util liveness function to improve check

--- sh/liveness.sh
+++ sh/liveness.sh
@@ -15,4 +15,17 @@
 # limitations under the License.
 ################################################################################
 
-exit 0
+HEALTH_FILE=/tmp/liveness
+AGE=${1:-'5'} #Default 5 minutes
+
+findFile=$(find "${HEALTH_FILE}" -mmin "-${AGE}" 2>/dev/null)
+if [ $? -ne 0 ]; then
+  echo "FAILING liveness probe. ${HEALTH_FILE} does not exist."
+  exit 1
+elif [ "${findFile}" = "" ]; then
+  echo "FAILING liveness probe. ${HEALTH_FILE} has not been touched in ${AGE} minutes."
+  exit 1
+else
+  # echo "file is good"
+  exit 0
+fi
