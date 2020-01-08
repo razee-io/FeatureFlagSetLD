2d0dcd34f0ca966d422ce0f527fd8b3ae3ca3f02
Alexander Lewitt <alewitt2@ibm.com>
2020-01-08 15:31:43 -0500

    Actual code to convert old yaml to new

--- src/conversionWebhookServer.js
+++ src/conversionWebhookServer.js
@@ -2,6 +2,7 @@ const express = require('express');
 const https = require('https');
 const http = require('http');
 const fs = require('fs');
+const clone = require('clone');
 const body_parser = require('body-parser');
 const objectPath = require('object-path');
 const log = require('./bunyan-api').createLogger('conversionWebhookServer');
@@ -20,23 +21,104 @@ app.get('/liveness', (req, res) => {
 });
 
 app.post('/crd-conversion', (req, res) => {
-  log.info(objectPath.get(req, 'body'));
+  log.debug(objectPath.get(req, 'body'));
 
-  let responseJson = {
-    'apiVersion': 'apiextensions.k8s.io/v1beta1',
-    'kind': 'ConversionReview'
-  };
-  objectPath.set(responseJson, 'response.uid', objectPath.get(req, 'body.request.uid'));
-  objectPath.set(responseJson, 'response.result', { 'status': 'Success' });
+  // Request
   let desiredVersion = objectPath.get(req, 'body.request.desiredAPIVersion');
-  let objs = objectPath.get(req, 'body.request.objects', []);
-  for (var i = 0; i < objs.length; i++) {
-    objectPath.set(objs, [i, 'apiVersion'], desiredVersion);
-    objectPath.set(objs, [i, 'metadata', 'annotations', 'razee'], 'you\'ve been converted');
+  let requestObjs = objectPath.get(req, 'body.request.objects', []);
+
+  // Response
+  let convertedObjs = [];
+  let responseJson = {
+    'apiVersion': objectPath.get(req, 'body.apiVersion'),
+    'kind': 'ConversionReview',
+    'response': {
+      'uid': objectPath.get(req, 'body.request.uid'),
+      'result': {
+        'status': 'Success'
+      }
+    }
+  };
+
+  // if there are no async/await requirements for conversion steps, change this to a forEach loop
+  for (var i = 0; i < requestObjs.length; i++) {
+    let requestObj = requestObjs[i];
+    let { convertedObj, conversionStatus } = convertResource(requestObj, desiredVersion);
+
+    // if a conversion step fails, set conversionStatus.status to 'Failed' with an optional message in conversionStatus.message
+    // Warning: Failing conversion can disrupt read and write access to the custom resources, including the ability to update or delete the resources. Conversion failures should be avoided whenever possible
+    objectPath.set(responseJson, 'response.result.status', conversionStatus.status);
+    objectPath.set(responseJson, 'response.result.message', conversionStatus.message);
+
+    // save converted object to final array
+    convertedObjs.push(convertedObj);
   }
-  objectPath.set(responseJson, 'response.convertedObjects', objs);
+  // attach final converted objects array to response
+  objectPath.set(responseJson, 'response.convertedObjects', convertedObjs);
+
+  log.debug(responseJson);
   res.json(responseJson);
 });
 
 https.createServer(credentials, app).listen(443, () => { log.info('Listening on port 443'); });
 http.createServer(app).listen(8080, () => { log.info('Listening on port 8080'); });
+
+
+// Conversion functions ========================================================
+
+function convertResource(requestObj, desiredVersion) {
+  let convertedObj = clone(requestObj);
+  let conversionStatus = { status: 'Success' };
+
+  // prep convertedObj
+  objectPath.del(convertedObj, 'spec');
+  objectPath.set(convertedObj, 'apiVersion', desiredVersion);
+
+  // do conversion steps
+  objectPath.set(convertedObj, 'metadata.annotations.razeedeploy', `This object has been converted from '${objectPath.get(requestObj, 'apiVersion')}' to '${desiredVersion}' by the RazeeDeploy Conversion Webhook Server. Please replace your currently saved resource with this one.`);
+  convertSdkKey(requestObj, convertedObj);
+  convertIdentity(requestObj, convertedObj);
+  convertIdentityKey(requestObj, convertedObj);
+
+  return { convertedObj, conversionStatus };
+}
+
+function convertSdkKey(requestObj, convertedObj) {
+  let sdkKey = objectPath.get(requestObj, 'spec.sdk-key');
+  if (typeof sdkKey == 'string') {
+    objectPath.set(convertedObj, 'spec.sdkKey', sdkKey);
+  } else if (typeof sdkKey == 'object') {
+    objectPath.set(convertedObj, 'spec.sdkKeyRef', sdkKey);
+  }
+}
+
+function convertIdentity(requestObj, convertedObj) {
+  let identity = objectPath.get(requestObj, 'spec.identity');
+  if (typeof identity == 'string') { // Get whole configmap via 'envFrom'
+    let envFromIdStr = { 'configMapRef': { 'name': identity } };
+    objectPath.push(convertedObj, 'spec.identityRef.envFrom', envFromIdStr);
+  } else if (Array.isArray(identity)) {
+    identity.forEach(id => {
+      if (typeof id == 'string') { // Get whole configmap via 'envFrom'
+        let envFromIdStr = { 'configMapRef': { 'name': id } };
+        objectPath.push(convertedObj, 'spec.identityRef.envFrom', envFromIdStr);
+      } else if (typeof id == 'object') {
+        let hasKey = objectPath.has(id, 'valueFrom.configMapKeyRef.key');
+        if (hasKey) { // Get single key from configmap via 'env'
+          let envIdObj = { 'name': objectPath.get(id, 'valueFrom.configMapKeyRef.key'), id };
+          objectPath.push(convertedObj, 'spec.identityRef.env', envIdObj);
+        } else { // Get whole configmap via 'envFrom'
+          let envFromIdObj = { 'configMapRef': { 'name': objectPath.get(id, 'valueFrom.configMapKeyRef.name'), 'namespace': objectPath.get(id, 'valueFrom.configMapKeyRef.name') } };
+          objectPath.push(convertedObj, 'spec.identityRef.envFrom', envFromIdObj);
+        }
+      }
+    });
+  }
+}
+
+function convertIdentityKey(requestObj, convertedObj) {
+  let identityKey = objectPath.get(requestObj, 'spec.identity-key');
+  objectPath.set(convertedObj, 'spec.identityKey', identityKey);
+}
+
+// End Conversion functions ====================================================
